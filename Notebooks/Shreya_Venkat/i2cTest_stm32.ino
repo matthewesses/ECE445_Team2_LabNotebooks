7716e96eeac1f746a62a1ebb2add8c16483e552e
shreya-v3 <156928022+shreya-v3@users.noreply.github.com>
2024-03-26 17:28:23 -0500

    move to different computer for testing

--- Notebooks/Shreya_Venkat/i2cTest_stm32.ino
+++ Notebooks/Shreya_Venkat/i2cTest_stm32.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 
 #define LIDAR_ADDRESS 0x62 // LiDAR's I2C address
+#define INIT_REG 0x00
+#define INIT_DATA 0x04
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,7 +13,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Wire.requestFrom(LIDAR_ADDRESS, 3);
+  Wire.beginTransmission(LIDAR_ADDRESS);
+  Wire.write(INIT_REG);
+  Wire.write(INIT_DATA);
+  Wire.endTransmission();
+
+  delay(10);
+  
+  Wire.requestFrom(LIDAR_ADDRESS, 1);
   while (Wire.available()) {
     char c = Wire.read();
     Serial.print(c);
