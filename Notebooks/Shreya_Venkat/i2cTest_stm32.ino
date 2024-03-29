9dddb9db8e2e90017fc31624a40804872668877c
shreya-v3 <156928022+shreya-v3@users.noreply.github.com>
2024-03-28 23:15:25 -0500

    we getting distances WOOO

--- Notebooks/Shreya_Venkat/i2cTest_stm32.ino
+++ Notebooks/Shreya_Venkat/i2cTest_stm32.ino
@@ -1,13 +1,9 @@
 #include <Wire.h>
 
 #define LIDAR_ADDRESS 0x62 // LiDAR's I2C address
-#
 #define INIT_REG 0x00
 #define INIT_DATA 0x04
-#define UPPER_DIST_REG 0x0F
-#define LOWER_DIST_REG 0x10
-
-bool initialized = false;
+#define TWO_BYTE 0x8f
 
 void setup() {
   // setup code
@@ -16,48 +12,36 @@ void setup() {
 }
 
 void loop() {
-  // attempt to initialize the LiDAR before communication
-  if (!initialized)
-  {
-    Wire.beginTransmission(LIDAR_ADDRESS);
-    Wire.write(INIT_REG);
-    Wire.write(INIT_DATA);
-    if (Wire.endTransmission() != 0)
-    {
-      Serial.println("Error: Initialization Failed");
-      delay(1000);
-      return;
-    }
-    else
-    {
-      Serial.println("Initialization Success");
-      initialized = true;
-    }
+
+  // initialize the LiDAR sensor to get accurate measurements
+  Wire.beginTransmission((int)LIDAR_ADDRESS);
+  Wire.write((int)INIT_REG);
+  Wire.write((int)INIT_DATA);
+  Wire.endTransmission();
+
+  delay(20);
+
+  // send the register that needs to be read from: 0x8f
+  Wire.beginTransmission((int)LIDAR_ADDRESS);
+  Wire.write((int)TWO_BYTE);
+  Wire.endTransmission();
+  
+  delay(20);
+   
+  // request data from the TWO_BYTE register
+  int distance = 0;
+  Wire.requestFrom((int)LIDAR_ADDRESS, 2);
+  if (Wire.available() >= 2) {
+     int upperByte = Wire.read(); //read upper 8 bits
+     upperByte = upperByte << 8; //shift to make room for lower 8 bits
+     int lowerByte = Wire.read(); //read lower 8 bits
+     distance = upperByte | lowerByte; //combine to create final distance value
+     Serial.println("Distance: " + String(distance) + "cm");
   }
   else
   {
-    Serial.println("Reached Part 2");
-    Wire.beginTransmission(LIDAR_ADDRESS);
-    Wire.write(UPPER_DIST_REG);
-    if (Wire.endTransmission() != 0)
-    {
-      Serial.println("Error: Read request failed");
-      delay(500);
-      return;
-    }
-    delay(40);
-    // once initialization is successful, request data
-    Wire.requestFrom(LIDAR_ADDRESS, 1);
-    if (Wire.available()) {
-      byte c = Wire.read();
-      Serial.print("Data read: ");
-      Serial.print(c);
-      Serial.println();
-    }
-    else
-    {
-      Serial.println("Error: Data not read");
-    }
+     Serial.println("Error: Distance could not be read");
   }
+  
   delay(800);
 }
