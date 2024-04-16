fdf72ebd1d20d4a0f28adbcbe09cfc2d90e1e988
shreya-v3 <156928022+shreya-v3@users.noreply.github.com>
2024-04-16 15:48:05 -0500

    added pin assignments

--- Notebooks/Shreya_Venkat/hall_effect_devboard.ino
+++ Notebooks/Shreya_Venkat/hall_effect_devboard.ino
@@ -1,8 +1,8 @@
 // represents all pins that are connected to hall-effect sensors
-int hallPins[] = {};
+int hallPins[] = {31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53};
 
 // represents all pins that are connected to motors
-int motorPins[] = {};
+int motorPins[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11};
 
 // represents all values received from hall effect sensors
 int hallVals[] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
@@ -31,6 +31,10 @@ void loop() {
   for (int i = 0; i < 12; i++)
   {
     motorVals[i] = ~(hallVals[i]);
+    if (motorVals[i] == 1)
+    {
+      Serial.println("Motor " + i + " is on!");
+    }
     digitalWrite(motorPins[i], motorVals[i]);
   }
   delay(1000);
