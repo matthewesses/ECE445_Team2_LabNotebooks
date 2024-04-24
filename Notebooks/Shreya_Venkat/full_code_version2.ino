4483a6b8144555d76aa576b5bffde595dd459e2a
shreya-v3 <156928022+shreya-v3@users.noreply.github.com>
2024-04-23 23:20:06 -0500

    code w/includes

--- Notebooks/Shreya_Venkat/full_code_version2.ino
+++ Notebooks/Shreya_Venkat/full_code_version2.ino
@@ -1,53 +1,69 @@
 #include <TinyStepper_28BYJ_48.h>
+#include <Wire.h>
+
+#define LIDAR_ADDRESS 0x62 // LiDAR's I2C address
+#define INIT_REG 0x00
+#define INIT_DATA 0x04
+#define TWO_BYTE 0x8f
 
 // array of haptic motors
-int motorPins[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11};
+int motorPins[] = {A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15};
 
 
-#define HALL_EFFECT_PIN
+//#define HALL_EFFECT_PIN
 
-const int MOTOR_PIN_IN1 = 2;
-const int MOTOR_PIN_IN2 = 3;
-const int MOTOR_PIN_IN3 = 4;
-const int MOTOR_PIN_IN4 = 5;
+//const int MOTOR_PIN_IN1 = 2;
+//const int MOTOR_PIN_IN2 = 3;
+//const int MOTOR_PIN_IN3 = 4;
+//const int MOTOR_PIN_IN4 = 5;
 
-const int rev_steps = 2048;
+//const int rev_steps = 2048;
 
-TinyStepper_28BYJ_48 stepper;
+//TinyStepper_28BYJ_48 stepper;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
-  stepper.connectToPins(MOTOR_PIN_IN1, MOTOR_PIN_IN2, MOTOR_PIN_IN3, MOTOR_PIN_IN4);
-  stepper.setSpeedInStepsPerSecond(256);
-  stepper.setAccelerationInStepsPerSecondPerSecond(512);
-//  int setCount = 10;
-//  digitalRead(HALL_EFFECT_PIN) == HIGH
-  while (digitalRead(HALL_EFFECT_PIN) == HIGH)
+  for (int i = 0; i < 12; i++)
   {
-    stepper.moveRelativeInSteps(1);
-//    setCount--;
+    pinMode(motorPins[i], OUTPUT);
+    analogWrite(motorPins[i], LOW);
   }
+  
+//  stepper.connectToPins(MOTOR_PIN_IN1, MOTOR_PIN_IN2, MOTOR_PIN_IN3, MOTOR_PIN_IN4);
+//  stepper.setSpeedInStepsPerSecond(256);
+//  stepper.setAccelerationInStepsPerSecondPerSecond(512);
+  
+  int setCount = 10;
+//  digitalRead(HALL_EFFECT_PIN) == HIGH
+//  while (setCount > 0)
+//  {
+//    stepper.moveRelativeInSteps(1);
+//    setCount--;
+//  }
 }
 
 void loop() {
-  stepper.setSpeedInStepsPerSecond(600);
-  stepper.setAccelerationInStepsPerSecondPerSecond(1000);
-//  stepper.moveRelativeInSteps(2048 * 5);
+//  stepper.setSpeedInStepsPerSecond(600);
+//  stepper.setAccelerationInStepsPerSecondPerSecond(1000);
   int loopCount = 0;
   while(loopCount < 12)
   {
-    stepper.moveRelativeInSteps(2048/12);
+//    stepper.moveRelativeInSteps(2048/12);
     // if lidar measurement <= 150, write high signal to motor
     int lidarDist = getLidarDistance();
+    Serial.println("Distance: " + String(lidarDist) + "cm");
     if (lidarDist <= 150)
     {
-      digitalWrite(motorPins[loopCount], HIGH);
-      delay(5000);
+      Serial.println("The motor should be on: " + String(loopCount));
+      delay(2000);
+      analogWrite(motorPins[loopCount], 100);
+      delay(2000);
     }
+    analogWrite(motorPins[loopCount], LOW);
     loopCount++;
-    digitalWrite(motorPins[loopCount], LOW);
+    delay(2000);
   }
   delay(2000);
   
